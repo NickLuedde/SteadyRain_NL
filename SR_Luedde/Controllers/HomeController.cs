e717fb61a894de12f862e6904a88902928e5506e
NICK LUEDDE <LUEDDENICHOLAS@GMAIL.COM>
2020-12-17 09:47:17 -0600

    Added the necessary controller & the view. Connected the DB in the controller.

--- SR_Luedde/Controllers/HomeController.cs
+++ SR_Luedde/Controllers/HomeController.cs
@@ -3,28 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SR_Luedde.Models;
 
 namespace SR_Luedde.Controllers
 {
     public class HomeController : Controller
     {
+
+        //I create a new object  
+        SR_StudentsEntities db = new SR_StudentsEntities();
+
         public ActionResult Index()
         {
+            //I Convert it into a list  
+
+            List<Student> StudentList = db.Students.ToList();
+            
+            //pass the StudentList list object to the view.  
+            return View(StudentList);
+
+        }
+
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
             return View();
         }
 
-        public ActionResult About()
+        // GET: Home/Create
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
         {
-            ViewBag.Message = "Your contact page.";
+            try
+            {
+                // TODO: Add insert logic here
 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
             return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }

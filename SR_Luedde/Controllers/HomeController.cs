c254049423c7202c830ce04ba8128ea055062c32
NICK LUEDDE <LUEDDENICHOLAS@GMAIL.COM>
2020-12-17 11:15:52 -0600

    Added Search Functionality,  radio buttons etc. and minor error handling.

--- SR_Luedde/Controllers/HomeController.cs
+++ SR_Luedde/Controllers/HomeController.cs
@@ -1,29 +1,46 @@
-﻿using System;
+﻿using SR_Luedde.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using SR_Luedde.Models;
 
 namespace SR_Luedde.Controllers
 {
     public class HomeController : Controller
     {
+        //I create a new object
+        private SR_StudentsEntities db = new SR_StudentsEntities();
 
-        //I create a new object  
-        SR_StudentsEntities db = new SR_StudentsEntities();
-
-        public ActionResult Index()
+        public ActionResult Index(string option, string search)
         {
-            //I Convert it into a list  
+            //I Convert it into a list
 
             List<Student> StudentList = db.Students.ToList();
-            
-            //pass the StudentList list object to the view.  
-            return View(StudentList);
 
+            if (option == "Name")
+            {
+                return View(db.Students.Where(x => x.Name == search || search == null).ToList());
+            }
+            if (option == "School")
+            {
+                return View(db.Students.Where(x => x.School == search || search == null).ToList());
+            }
+            if (option == "Major")
+            {
+                return View(db.Students.Where(x => x.Major == search || search == null).ToList());
+            }
+            if (option == "Date")
+            {
+                return View(db.Students.Where(x => x.Date == search || search == null).ToList());
+            }
+            if (option == "Active")
+            {
+                return View(db.Students.Where(x => x.Active == search || search == null).ToList());
+            }
+            else
+            {
+                return View(db.Students.Where(x => x.Name.StartsWith(search) || search == null).ToList());
+            }
         }
-
 
         // GET: Home/Details/5
         public ActionResult Details(int id)
